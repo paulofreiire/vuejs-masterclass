8932e01ae41309d532da4da8a22bf91c666867e2
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-05 16:40:19 -0300

    43

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,10 +3,9 @@ import { supabase } from '@/lib/supbaseClient'
 import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types.ts'
 import type { ColumnDef } from '@tanstack/vue-table'
-import { data } from 'autoprefixer'
 import DataTable from '@/components/ui/data-table/DataTable.vue'
 
-const tasks = ref<Tables<'projects'>[] | null>([])
+const tasks = ref<Tables<'projects'>[] | null>(null)
 ;(async () => {
   const { data, error } = await supabase.from('projects').select()
   if (error) console.log(error)
@@ -14,48 +13,17 @@ const tasks = ref<Tables<'projects'>[] | null>([])
   tasks.value = data
 })()
 
-interface Payment {
-  id: string
-  amount: number
-  status: 'pending' | 'processing' | 'success' | 'failed'
-  email: string
-}
-
-const payments: Payment[] = [
+const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '489e1d42',
-    amount: 125,
-    status: 'processing',
-    email: 'example@gmail.com',
-  },
-  // ...
-]
-const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'amount',
-    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left ' }, 'Name'),
     cell: ({ row }) => {
-      const amount = Number.parseFloat(row.getValue('amount'))
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount)
-
-      return h('div', { class: 'text-right font-medium' }, formatted)
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
     },
-  }
+  },
 ]
 </script>
 
 <template>
-  <DataTable :columns="columns" :data="payments" />
+  <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
-
-<style scoped></style>
-^
