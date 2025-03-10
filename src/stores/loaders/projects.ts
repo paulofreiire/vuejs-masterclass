eec931a4e0703bb18ca2a44a5a20ba51b201c673
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-10 20:02:38 -0300

    9 5

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,14 +1,16 @@
 import { projectsQuery } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 import type { Projects } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>([])
-  const getProjects = async () => {
-    if (projects.value?.length) return
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
-    const { data, error, status } = await projectsQuery
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
+
     projects.value = data
   }
   return { projects, getProjects }
