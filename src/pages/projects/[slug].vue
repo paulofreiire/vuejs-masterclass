f546661252fa53430ea70c1647a589c2a35f5850
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-11 13:36:42 -0300

    107

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -20,7 +20,7 @@ await getProject(slug)
     <TableRow>
       <TableHead> Name</TableHead>
       <TableCell>
-        <AppInPlaceEditText v-model="project.name" />
+        <AppInPlaceEditText v-model="project.name" @commit="console.log('fon')"/>
       </TableCell>
     </TableRow>
     <TableRow>
