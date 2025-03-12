88a7a6cf6e839b7403cbd7f54b067b2054d94a08
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-12 10:35:47 -0300

    113

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -31,7 +31,7 @@ const collabs = project.value?.collaborators
     <TableRow>
       <TableHead> Description</TableHead>
       <TableCell>
-        <AppInPlaceEditText v-model="project.description" @commit="updateProject" />
+        <AppInPlaceEditTextArea v-model="project.description" @commit="updateProject" />
       </TableCell>
     </TableRow>
     <TableRow>
