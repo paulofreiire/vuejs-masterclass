8a98245be0387e711659189faab89426dd15113f
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-12 11:33:41 -0300

    117

--- src/components/AppNew/AppNewTask.vue
+++ src/components/AppNew/AppNewTask.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import type { CreateNewTask } from '@/types/CreateNewForm.ts'
+
 const sheetOpen = defineModel<boolean>()
+
+const createTask = async (formData: CreateNewTask) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(formData)
+    }, 1000)
+  })
+}
 </script>
 
 <template>
@@ -8,7 +18,9 @@ const sheetOpen = defineModel<boolean>()
       <SheetHeader>
         <SheetTitle>Create new task</SheetTitle>
       </SheetHeader>
-      <FormKit type="text" />
+      <FormKit type="form" @submit="createTask" submit-label="Create task">
+        <FormKit type="text" name="name" id="Name" placeholder="My new task" />
+      </FormKit>
     </SheetContent>
   </Sheet>
 </template>
