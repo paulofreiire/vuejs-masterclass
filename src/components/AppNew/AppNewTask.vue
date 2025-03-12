39c6e02cca5eca03d7a47c266fdce27e5149c11c
Paulo Freire <paulo.freiire@hotmail.com>
2025-03-12 11:04:58 -0300

    115

---
+++ src/components/AppNew/AppNewTask.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const sheetOpen = defineModel<boolean>()
+</script>
+
+<template>
+  <Sheet v-model:open="sheetOpen">
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Create new task</SheetTitle>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+</template>
